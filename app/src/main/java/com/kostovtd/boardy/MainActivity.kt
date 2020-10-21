6a583543d32cb648b8dd80e9adb1d1cf8f726bd7
Todor Kostov <todor.kostov@scaleforce.io>
2020-10-21 09:27:26 +0300

    Firebase initial integration

---
+++ app/src/main/java/com/kostovtd/boardy/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.kostovtd.boardy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by tosheto on 21.10.20.
+ */
+class MainActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
