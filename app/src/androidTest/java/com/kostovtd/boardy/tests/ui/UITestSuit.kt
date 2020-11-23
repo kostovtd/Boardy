d029923872471cbb35e7400930081fa5bb652cfa
Todor Kostov <todor.kostov@scaleforce.io>
2020-11-23 10:02:39 +0200

    BRDY-5: Try to run UI tests on GitHub Actions

---
+++ app/src/androidTest/java/com/kostovtd/boardy/tests/ui/UITestSuit.kt
@@ -0,0 +1,12 @@
+package com.kostovtd.boardy.tests.ui
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * Created by tosheto on 23.11.20.
+ */
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    SignInUITests::class)
+class UITestSuit
